ea43adb9619442f09dfadc0fd28648dc2f59d149
samuel2935 <samuelblessed38@gmail.com>
2026-05-17 20:07:38 +0100

    feat: update

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -1,20 +1,20 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 console.log('DB DEBUG:', {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
 });
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'Coldpath@2935',
-  database: 'users',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+  username: process.env.DATABASE_USER || 'postgres',
+  password: process.env.DATABASE_PASSWORD || '',
+  database: process.env.DATABASE_NAME || 'users',
   autoLoadEntities: true,
-  synchronize: true,
+  synchronize: false,
 };
