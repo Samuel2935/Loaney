2ef29a4663b169e4646da5502baaaac79ffbd7e4
samuel2935 <samuelblessed38@gmail.com>
2026-05-31 16:01:04 +0100

    feat: add prod db url

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,3 +1,47 @@
+// import * as dotenv from 'dotenv';
+// dotenv.config();
+
+// import { DataSource } from 'typeorm';
+
+// export const getTypeOrmConfig = () => ({
+//   type: 'postgres' as const,
+
+//   host: process.env.DB_HOST,
+
+//   port: Number(process.env.DB_PORT),
+
+//   username: process.env.DB_USER,
+
+//   password: process.env.DB_PASS,
+
+//   database: process.env.DB_NAME,
+
+//   entities: ['dist/**/*.entity.js'],
+
+//   migrations: ['dist/database/migrations/*.js'],
+
+//   synchronize: false,
+// });
+
+// export const AppDataSource = new DataSource({
+//   type: 'postgres',
+
+//   host: process.env.DB_HOST,
+
+//   port: Number(process.env.DB_PORT),
+
+//   username: process.env.DB_USER,
+
+//   password: process.env.DB_PASS,
+
+//   database: process.env.DB_NAME,
+
+//   entities: ['src/**/*.entity.ts'],
+
+//   migrations: ['src/database/migrations/*.ts'],
+
+//   synchronize: false,
+// });
 import * as dotenv from 'dotenv';
 dotenv.config();
 
@@ -6,15 +50,14 @@ import { DataSource } from 'typeorm';
 export const getTypeOrmConfig = () => ({
   type: 'postgres' as const,
 
-  host: process.env.DB_HOST,
+  url: process.env.DATABASE_URL,
 
-  port: Number(process.env.DB_PORT),
-
-  username: process.env.DB_USER,
-
-  password: process.env.DB_PASS,
-
-  database: process.env.DB_NAME,
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? {
+          rejectUnauthorized: false,
+        }
+      : false,
 
   entities: ['dist/**/*.entity.js'],
 
@@ -26,15 +69,14 @@ export const getTypeOrmConfig = () => ({
 export const AppDataSource = new DataSource({
   type: 'postgres',
 
-  host: process.env.DB_HOST,
+  url: process.env.DATABASE_URL,
 
-  port: Number(process.env.DB_PORT),
-
-  username: process.env.DB_USER,
-
-  password: process.env.DB_PASS,
-
-  database: process.env.DB_NAME,
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? {
+          rejectUnauthorized: false,
+        }
+      : false,
 
   entities: ['src/**/*.entity.ts'],
 
--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -1,20 +1,25 @@
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 console.log('DB DEBUG:', {
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  databaseUrlExists: !!process.env.DATABASE_URL,
+  nodeEnv: process.env.NODE_ENV,
 });
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: parseInt(process.env.DATABASE_PORT || '5432', 10),
-  username: process.env.DATABASE_USER || 'postgres',
-  password: process.env.DATABASE_PASSWORD || '',
-  database: process.env.DATABASE_NAME || 'users',
+
+  url: process.env.DATABASE_URL,
+
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? {
+          rejectUnauthorized: false,
+        }
+      : false,
+
   autoLoadEntities: true,
+
   synchronize: false,
 };
